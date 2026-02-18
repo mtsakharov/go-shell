ff20928a99fb7be88edb2e109f10683f8fa137e5
Movsar Tsakharov <mtsakharov@gmail.com>
2026-02-18 23:47:25 +0500

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -21,6 +21,11 @@ func main() {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
+
+		if command == "exit" {
+			break
+		}
+
 		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
 }
